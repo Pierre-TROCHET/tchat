fbe37318eee25332da943a6c33fde5b41fdc08c3
Pierre-François TROCHET <Pierre-Francois.Trochet@upmc.fr>
2020-12-15 09:43:30 +0100

    amélioration du routing

--- src/app/guards/tchat.guard.ts
+++ src/app/guards/tchat.guard.ts
@@ -14,7 +14,7 @@ export class TchatGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if(!this.tokenService.getToken()){
-        this.router.navigate(["/login"]);
+        this.router.navigate(["/home"]);
         return false;
       }
     return true;
